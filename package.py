0bec544a80add64ecc63bbee3c5f9e146b758a87
lixk <lixk@akulaku.com>
2018-10-18 15:02:22 +0800

    update

--- package.py
+++ package.py
@@ -3,7 +3,7 @@ from PyInstaller.__main__ import run
 import server
 
 if __name__ == '__main__':
-    opts = ['window.py', '-F', '-w', '--icon=bin/favicon.ico', '-y', '--distpath=out', '--specpath=out']
+    opts = ['window.py', '-F', '-w', '--icon=favicon.ico', '-y', '--distpath=out/dist', '--specpath=out']
     server.init_service()
     for service_name in server.SERVICE.keys():
         opts.append('--hidden-import=%s' % service_name[0: service_name.rfind('.')])
