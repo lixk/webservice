35c0bec769dcb35d7e4e238b61c3183d064cbb73
lixk <lixk@akulaku.com>
2018-10-24 15:36:38 +0800

    update

--- package.py
+++ package.py
@@ -1,11 +1,11 @@
 from PyInstaller.__main__ import run
 
-import server
+import py4js
 
 if __name__ == '__main__':
     opts = ['window.py', '-F', '-w', '--icon=favicon.ico', '-y', '--distpath=out/dist', '--specpath=out']
-    server.init_service()
-    for service_name in server.SERVICE.keys():
+    py4js.init_service()
+    for service_name in py4js.SERVICE.keys():
         opts.append('--hidden-import=%s' % service_name[0: service_name.rfind('.')])
 
     run(opts)
--- service/executor.py
+++ service/executor.py
@@ -1,3 +1,7 @@
+import time
+
+
 def add(x, y):
+    time.sleep(6)
     return float(x) + float(y)
 
